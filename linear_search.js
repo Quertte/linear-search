cb1e0b2c9f5fdbb856e354211f7c79c0c1acf6c2
Quertte <a.injen90@gmail.com>
2022-08-10 17:02:42 +0300

    linear and global linear search

--- linear_search.js
+++ linear_search.js
@@ -1,11 +1,21 @@
 //Реализуй функции линейного поиска. Проверь результат тестами.
 
-function linearSearch() {
-  
+function linearSearch(elem, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === elem) {
+      return i;
+    }
+  }
 }
 
-function globalLinearSearch() {
-  
+function globalLinearSearch(elem, array) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === elem) {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 module.exports = {
